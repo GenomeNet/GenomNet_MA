955e71677ad116e9d20c2491cd5e41a6a193a675
mb706 <things.developer@mb706.com>
2022-02-21 00:20:33 +0100

    cleaning up

--- nas_darts/comp_aux.py
+++ nas_darts/comp_aux.py
@@ -101,15 +101,7 @@ def get_w_range(cnn_steps, switch_cnn):
 
 
 
-#idxs[0][0]
-#alphas_normal[0:2]
-
-#alphas_normal[idxs[i][0]:idxs[i][1]]
-
-
 # auxiliary functions to make RHN part faster and enable continuous weight sharing as well as discarded edges
-
-
 
 def get_disc_edges(rnn_steps, switch_rnn):
     offset = 0
@@ -135,9 +127,6 @@ def get_disc_edges(rnn_steps, switch_rnn):
 
 
 
-
-# switch_rnn = switches_rnn
-# switch_rnn[25][0] = True
 def activ_fun(rnn_steps, PRIMITIVES_rnn, switch_rnn):
     #
     act_all = []
@@ -169,11 +158,6 @@ def activ_fun(rnn_steps, PRIMITIVES_rnn, switch_rnn):
     return act_all, act_nums
 
           
-      
-#acn, acf = activ_fun(rnn_steps, PRIMITIVES_rnn, switch_rnn) 
-
-
-
 
 def compute_positions(switch_rnn, rnn_steps, PRIMITIVES_rnn):
     
@@ -183,24 +167,21 @@ def compute_positions(switch_rnn, rnn_steps, PRIMITIVES_rnn):
 
 
     positions = []
-    # step_position = []
+
     offset = 0
     rows = []
     cols = []
     nodes = []
     
     for i in range(len(acf)):
-        # i=3
         
         for k in acf[i]:
-            # k=4
             
             row = []
             col = []
             nod = []
             
             for j in range(offset, offset+i+1):
-                # j=9
                 # j: 0 (offset=0)
                 # j: 1,2 (offset=1)
                 # j: 3,4,5 (offset=3)
@@ -221,107 +202,3 @@ def compute_positions(switch_rnn, rnn_steps, PRIMITIVES_rnn):
 
 
 
-
-
-
-# rows, nodes, cols = compute_positions(switch_rnn, rnn_steps)
-
-
-#x_mask = h_mask = x = h_prev = torch.rand(2,256)
-#probs = new_rnn_arch
-#s0 = torch.rand(2,256)
-# states_original = states
-#cnt = 0     
-#offset = 0
-#states = s0.unsqueeze(0) 
-#disc_edge_count = 0
-
-#for i in range(rnn_steps):
-    # i=7
-#    masked_states = states
- 
-#    ch = masked_states.view(-1, nhid).mm(_Ws[i]).view(i+1, -1, 2*nhid) 
-    
-#    c, h = torch.split(ch, nhid, dim=-1)
-       
-#    c = c.sigmoid()
-    
-#    s = torch.zeros_like(s0) # [2,256]
-    #s = s.unsqueeze(0)
-    #unweighteds = [states + c * (torch.tanh(h) - states), states + c * (torch.sigmoid(h) - states), states + c * (torch.relu(h) - states), states + c * (h - states)]
-
-#    for name in acn[i]: # geht von 0:4
-        # k=4
-        #print(name)
-        
-        # name = PRIMITIVES_rnn[k]
-        
-        #if name == 'none':
-        #  continue
-#        fn = _get_activation(name)
-        # fn = _get_activation(name)
-
-#        unweighted = states + c * (fn(h) - states) # states [3,2,256], wobei s immer [2,256]
-          
-#        s += torch.sum(probs[rows[cnt], cols[cnt]].unsqueeze(-1).unsqueeze(-1) * unweighted[nodes[cnt], :, :], dim=0) 
-        # sm = torch.sum(probs[rows[cnt], cols[cnt]].unsqueeze(-1).unsqueeze(-1) * unweighted, dim=0) 
-        
-#        cnt += 1 
-
-#    states = torch.cat([states, s.unsqueeze(0)], 0) # von [1,2,256] zu [2,2,256]
-
-
-
-#w = torch.rand(3,1,1)
-#w[0] = 1
-#A = torch.rand(3,2,4)
-#sm = torch.sum(w[[0,2],:] * A[[0,2],:,:], dim=0)  # 
-
-#s1 = w[1]*A[0]
-#s2 = w[1]*A[1]
-#s3 = w[1]*A[2]
-#sm_new = s1+s2+s3
-
-
-# states_mein = states
-# states = states_original
-   
-#offset = 0
-    
-#for i in range(rnn_steps):
-
-#  masked_states = states
-  
-#  ch = masked_states.view(-1, nhid).mm(_Ws[i]).view(i+1, -1, 2*nhid) 
-#  c, h = torch.split(ch, nhid, dim=-1)
-   
-#  c = c.sigmoid()
-
-#  s = torch.zeros_like(s0) 
-  #s = s.unsqueeze(0)
-
-#  for k, name in enumerate(PRIMITIVES_rnn): # geht von 0:4
-      # switch_rnn = switches_rnn[0]
-      # k=4
-      #print('ctn')
-      #print(count_false)
-      #if rnn_switch_row[k]:
-        
-    #name = PRIMITIVES_rnn[k]
-        
-#    if name == 'none':
-#        continue
-#    fn = _get_activation(name)
-    # fn = _get_activation(name)
-
-#    unweighted = states + c * (fn(h) - states)
-          
-#    s += torch.sum(probs[offset:offset+i+1, k].unsqueeze(-1).unsqueeze(-1) * unweighted, dim=0)  
-    #s += torch.sum(probs[[28,29,39], [0,1,0]].unsqueeze(-1).unsqueeze(-1) * unweighted, dim=0)  
-    #s += sum(probs[row, k].unsqueeze(-1).unsqueeze(-1) * unweighted for row, k in zip(rows[i], cols[i]))
-
-    # else:
-     #  count_false += 1
-    
-#  states = torch.cat([states, s.unsqueeze(0)], 0) 
-#  offset += i+1
